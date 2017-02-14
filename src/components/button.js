742903bdc5f9e6264803ff15375c43f19f779fa1
Jason Leong <soundslikejason@users.noreply.github.com>
2017-02-13 16:41:23 -0800

    explaining PropTypes

--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 const Button = (props) => (
   <button>{props.name}</button>
 );
+
+Button.propTypes = {
+  name: PropTypes.string,
+};
 
 export default Button;
