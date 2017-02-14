a7fa82476c0452fc3d3691253af86df6c1dc7b53
Jason Leong <soundslikejason@users.noreply.github.com>
2017-02-13 16:19:06 -0800

    react component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Button from './button';
 
 // () => fat arrow function set up by es6, function() {}
 const App = () => (
   <div>
-    HI
+    <Button />
   </div>
 );
 
